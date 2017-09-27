a04836e63d520b8246abfc88e160d9fa76ec6b2b
Tim Robinson <tim@panubo.com>
2017-09-27 19:18:29 +1000

    Update dockerFind

--- vars/dockerFind.groovy
+++ vars/dockerFind.groovy
@@ -1,10 +1,12 @@
 def call() {
     def dockerBuilds = []
-    def utils = new com.panubo.Utils()
+    // def utils = new com.panubo.Utils()
     if (fileExists('Dockerfile')) {
         dockerBuilds = ['.']
     } else {
-        dockerBuilds = utils.readLines(sh(returnStdout: true, script: 'find . -maxdepth 2 -name Dockerfile -exec dirname {} \\;'))
+        dockerBuilds = sh(returnStdout: true, script: 'find . -maxdepth 2 -name Dockerfile -exec dirname {} \\;')
+            .trim()
+            .split(System.getProperty("line.separator"))
     }
     return dockerBuilds
 }
